d851b7ad7418d1543ccd97c17c4a85efe6a9557b
Scott Harrison <scottrules44@gmail.com>
2022-09-10 21:36:12 -0500

    Update to adapter for Max 11.4

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = {  },
-		frameworks = { "AppLovinMediationHyprMXAdapter", "HyprMX" },
+		staticLibs = {"HyprMXAdapter"},
+		frameworks = { "HyprMX" },
 		frameworksOptional = {},
 	},
 }
