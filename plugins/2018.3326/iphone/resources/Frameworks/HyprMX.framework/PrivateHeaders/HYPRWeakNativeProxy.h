d6e162afe52ee7e3eeac3ad7380e1e91d420e989
Scott Harrison <scottrules44@gmail.com>
2025-05-17 15:31:42 -0500

    13.2 Update

--- plugins/2018.3326/iphone/resources/Frameworks/HyprMX.framework/PrivateHeaders/HYPRWeakNativeProxy.h
+++ plugins/2018.3326/iphone/resources/Frameworks/HyprMX.framework/PrivateHeaders/HYPRWeakNativeProxy.h
@@ -7,12 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HYPRFullscreenNativeDelegate.h"
+#import "HYPRWebTrafficNativeDelegate.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HYPRWeakNativeProxy : NSObject
-@property (weak, nonatomic) NSObject *delegate;
+@protocol HYPRWeakNativeProxyProtocol <NSObject>
 -(instancetype)initWithDelegate:(NSObject *)delegate;
 @end
 
 NS_ASSUME_NONNULL_END
+
+@interface HYPRWeakNativeProxy: NSObject <HYPRWeakNativeProxyProtocol>
+@end
+
+@interface HYPRFullscreenWeakNativeProxy: HYPRWeakNativeProxy <HYPRFullscreenNativeDelegate>
+@end
+
+@interface HYPRWebTrafficWeakNativeProxy: HYPRFullscreenWeakNativeProxy <HYPRWebTrafficNativeDelegate>
+@end
